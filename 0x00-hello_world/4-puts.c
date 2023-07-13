b6b3cf0455cfb3709bc6c1659be7f2c947b5c714
ViweTeko <iamviwe.teko@gmail.com>
2023-07-13 13:12:46 -0700

    Puts instead of printf

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ *  Basically writing a program that prints a message 
+ *  using puts instead of printf
+ */
+
+int main(void)
+{
+  puts("/"Programming is like building a multilingual puzzle");
+  return 0;
+}
