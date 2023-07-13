0d3c6f18cd702d73dfec55dfd32f5854b767f984
ViweTeko <iamviwe.teko@gmail.com>
2023-07-13 14:03:12 -0700

    puts instead of printf

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
 
 /**
- * main - prints a line using the puts function.
- * 
+ * main - prints a line using the puts function
+ *
  * Return: 0
  */
 
 int main(void)
 {
-  puts("Programming is like building a multilingual puzzle");
-  return 0;
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
 }
