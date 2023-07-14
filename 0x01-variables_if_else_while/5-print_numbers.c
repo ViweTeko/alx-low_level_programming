076b4b4aaaf570e4b51b4b1726f3c5d012b9f335
ViweTeko <iamviwe.teko@gmail.com>
2023-07-14 11:07:21 -0700

    Printing numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - displaying numbers
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	int x = 0;
+
+	while (x <= 9)
+	{
+		printf("%i", x);
+		x++;
+	}
+
+	printf("\n");
+
+	return (0);
+}
