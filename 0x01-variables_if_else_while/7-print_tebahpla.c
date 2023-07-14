2a5730cfa6741e47f34e858ce771dcbeda6a102c
ViweTeko <iamviwe.teko@gmail.com>
2023-07-14 11:38:33 -0700

    Printing in reverse

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - displays lower case alphabet in reverse order
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	char wol;
+
+	for (wol = 'z'; wol > 'a'; wol--)
+	{
+		putchar(wol);
+	}
+
+	putchar("\n");
+
+	return (0);
+}
