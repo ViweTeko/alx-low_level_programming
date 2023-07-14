1e69deabca2510eb04b7dc55dbbe84ac7e1d1ace
ViweTeko <iamviwe.teko@gmail.com>
2023-07-14 09:55:16 -0700

    lower and UPPER cases

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -10,11 +10,13 @@ int main(void)
 {
 	char low, UP;
 
+	/* lower case */
 	for (low = 'a'; low <= 'z'; low++)
 	{
 		putchar(low);
 	}
 
+	/* UPPER CASE */
 	for (UP = 'A'; UP <= 'Z'; UP++)
 	{
 		putchar(UP);
