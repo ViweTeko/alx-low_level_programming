59b32dcdafe13b65ac0cc80fcf8785e7bec0f8d0
ViweTeko <iamviwe.teko@gmail.com>
2023-07-18 11:41:23 -0700

    Abs: not what you think..

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _abs - computes absolute value of integer
+ *
+ * @z: the integer
+ *
+ * Return: 0
+ */
+
+int _abs(int z)
+{
+	if (z < 0)
+	{
+		z = (-1) * z;
+
+		return (z);
+	}
+}
