20fd16e55c2f3e698254122b03cdf1d430263013
ViweTeko <iamviwe.teko@gmail.com>
2023-07-18 14:57:30 -0700

    Fibonacci

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * main - fibonacci
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	int a;
+	unsigned long int b = 0, c = 1, d;
+	for (a = 1; a <= 49; ++a)
+	{
+		d = b + c;
+		b = c;
+		c = d;
+
+		printf("%lu, ", d);
+	}
+	printf("%lu\n", d + b);
+
+	return (0);
+}
