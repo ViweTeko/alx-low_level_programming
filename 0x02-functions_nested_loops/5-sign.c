875c8a54451c62812c23fcd74265df6e0bbdaa93
ViweTeko <iamviwe.teko@gmail.com>
2023-07-18 10:05:07 -0700

    Sign

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * print_sign - prints sign of number
+ *
+ * @n: displays number sign
+ *
+ * Return: 1 if positive number, 0 if zero, -1 if less than zero
+ */
+
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+	else if (n == 0)
+	{
+		_putchar('0');
+		return (0);
+	}
+	else (n < 0)
+	{
+		_putchar('-');
+		return (-1);
+	}
+}
