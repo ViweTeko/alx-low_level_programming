4dab856cc24cb552924f628bcdcff168ac0a188f
ViweTeko <iamviwe.teko@gmail.com>
2023-07-18 13:31:36 -0700

    nine times

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * times_table - shows 9 times table
+ *
+ * Return: 0
+ */
+
+void times_table(void)
+{
+	int d, e, f;
+
+	for (d = 0; d <= 9; ++d)
+	{
+		_putchar('0');
+		for (e = 0; e <= 9; ++e)
+		{
+			_putchar(',');
+			_putchar(' ');
+
+			f = d * e;
+
+			if (f > 9)
+				_putchar((f / 10) + '0');
+			else
+				_putchar(' ');
+
+			_putchar((f % 10) + '0');
+		}
+		_putchar('\n');
+	}
+}
