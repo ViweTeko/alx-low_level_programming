20740fce6deefe4b479c4f17c44a851e6ba8b11f
ViweTeko <iamviwe.teko@gmail.com>
2023-07-18 14:33:59 -0700

    Times

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -8,35 +8,15 @@
 
 void print_times_table(int s)
 {
-	int a, b, c;
+	int a, b;
 
-	if (s <= 15 && s <= 0)
+	if (s > 15 && s < 0)
 	{
 		for (a = 0; a <= s; ++a)
 		{
-			_putchar('0');
-
 			for (b = 0; b <= s; ++b)
 			{
-				_putchar(',');
-				_putchar(' ');
-
-				c = a * b;
-
-				if (c <= 9)
-					_putchar(' ');
-				if (c <= 99)
-					_putchar(' ');
-				if (c >= 100)
-				{
-					_putchar((c / 100) + '0');
-					_putchar((c / 10) % 10 + '0');
-				}
-				else if (c <= 99 && c >= 10)
-				{
-					_putchar((c / 10) + '0');
-				}
-				_putchar((c % 10) + '0');
+				_putchar('0' + a * b);
 			}
 			_putchar('\n');
 		}
