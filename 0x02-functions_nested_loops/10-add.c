9b436dea27a87489962fd20f89be9160fe30cefb
ViweTeko <iamviwe.teko@gmail.com>
2023-07-18 13:40:52 -0700

    Adding integers

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * add - adding two integers
+ *
+ * @m: first input
+ * @n: second input
+ *
+ * Return: m + n
+ */
+
+int add(int m, int n)
+{
+	int l = m + n;
+
+	return (l);
+}
