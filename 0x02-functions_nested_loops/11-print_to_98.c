b1aa1801ee24f97bb6eab5193e63922886d71e08
ViweTeko <iamviwe.teko@gmail.com>
2023-07-18 13:52:20 -0700

    Printing

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_to_98 - printing to 98 in order
+ *
+ * @h: output
+ */
+
+void print_to_98(int h)
+{
+	if (h < 98)
+		for (h = h; h <= 97; ++h)
+			printf("%d, ", h);
+	else
+		for (h = h; h > 98; --h)
+			printf("%d, ", h);
+	printf('98\n');
+}
