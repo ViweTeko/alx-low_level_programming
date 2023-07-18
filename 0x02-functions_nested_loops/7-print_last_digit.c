3f2731c614fc8b4b22ed602f0969baf624e0525d
ViweTeko <iamviwe.teko@gmail.com>
2023-07-18 11:57:24 -0700

    Last digit

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_last_digit - prints the last digit
+ *
+ * @last: prints last digit
+ *
+ * Return: last digit
+ */
+
+int print_last_digit(int last)
+{
+	last = last % 10;
+
+	if (last < 0)
+		last = -last;
+	_putchar(n + '0');
+
+	return (last);
+}
