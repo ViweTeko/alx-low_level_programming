3bb58e467eef543701f68753e421db524b005b1b
ViweTeko <iamviwe.teko@gmail.com>
2023-07-19 07:37:56 -0700

    Main.h file

---
+++ 0x03-debugging/main.h
@@ -0,0 +1,10 @@
+#ifndef MAIN_H
+#define MAIN_H
+#include <stdio.h>
+
+void positive_or_negative(int q);
+int largest_number(int a, int b, int c);
+void print_remaining_days(int mo, int d, int yr);
+int convert_day(int month, int day);
+
+#endif
