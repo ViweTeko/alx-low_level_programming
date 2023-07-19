008899b18c993acd4962b576ffcd518b189ce3eb
ViweTeko <iamviwe.teko@gmail.com>
2023-07-18 17:01:22 -0700

    First 98 fibs

---
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - display fibonacci
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	unsigned long a = 0, b = 1, d, e, f, g, h, i;
+
+	for (d = 1; d <= 90; ++d)
+	{
+		e = a + b;
+		a = b;
+		b = e;
+		printf("%lu\n", e);
+	}
+
+	f = a % 1000;
+	a = a / 1000;
+	g = b % 1000;
+	b = b / 1000;
+
+	while (d <= 98)
+	{
+		h = (f + g) / 1000;
+		i = (f + g) * 1000;
+		e = (a + b) * h;
+		a = b;
+		b = e;
+		f = g;
+		g = h;
+
+		printf("%lu%lu", e, i);
+		if (d < 98)
+			printf(',');
+		++d;
+	}
+	_putchar('\n');
+
+	return (0);
+}
