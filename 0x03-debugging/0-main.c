70dec2b5b5fb6910e15418c8f59012ab4d61c5fe
ViweTeko <iamviwe.teko@gmail.com>
2023-07-19 08:53:19 -0700

    Main 1

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,18 +1,16 @@
 #include "main.h"
 
 /**
- * main - test function
- *
- * Return: 0
- */
+* main - tests function that prints if integer is positive or negative
+* Return: 0
+*/
 
 int main(void)
 {
-	int q;
-		
-	q = 98 % 2;
+        int i;
 
-	positive_or_negative(q);
+        i = 98 % 2;
+        positive_or_negative(i);
 
-	return (0);
+        return (0);
 }
