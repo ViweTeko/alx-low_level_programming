05415e6d799336be315b2cb3491514a8c0d75702
ViweTeko <iamviwe.teko@gmail.com>
2023-07-20 04:39:03 -0700

    Printing most numbers

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -12,8 +12,12 @@ void print_most_numbers(void)
 
 	for (a = '0'; a <= '9'; ++a)
 	{
-		if (a != '2' || a != '4') {
-			_putchar(a);
+		if (a != '2') 
+		{
+			if (a != 4)
+			{
+				_putchar(a);
+			}
 		}
 	}
 	_putchar('/n');
