9cda42b4176c43c68690c764b1ce813f40298c6d
ViweTeko <iamviwe.teko@gmail.com>
2023-07-20 04:22:22 -0700

    Printing numbers

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_numbers - prints numbers
+ *
+ * Return: 0
+ */
+
+void print_numbers(void)
+{
+	int z, a;
+
+	for (a = 0; a <=9; ++a)
+	{
+		_putchar(a);
+	}
+
+	_putchar('\n');
+}
