7d04b14705c2cbef543ac01e9b77a44ac8ba0695
ViweTeko <iamviwe.teko@gmail.com>
2023-07-20 22:41:49 -0700

    Fizz Buzz fixed

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -11,13 +11,13 @@ int main(void)
 {
 	int a;
 
-	for (a = 0; a <= 100; ++a)
+	for (a = 1; a <= 100; ++a)
 	{
 		if (a % 3 == 0)
 			printf("Fizz");
-		if (a % 5 == 0)
+		else if (a % 5 == 0)
 			printf("Buzz");
-		if (a % 3 == 0 && a % 5 ==0)
+		else if (a % 3 == 0 && a % 5 ==0)
 			printf("FizzBuzz");
 		else
 			printf("%d", a)
