a045a4b2a8adc4fa0ca48b176a7b9599027e325e
ViweTeko <iamviwe.teko@gmail.com>
2023-07-20 20:39:34 -0700

    Print square

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_square - prints a square
+ *
+ * @size: size of the square
+ */
+
+void print_square(int size)
+{
+	int a, b;
+
+	for (a = 0; a < size; ++a)
+	{
+		for (b = 0; b < size; ++b)
+			_putchar('#');
+		_putchar('\n');
+	}
+
+	if (size <= 0)
+		_putchar('\n');
+}
