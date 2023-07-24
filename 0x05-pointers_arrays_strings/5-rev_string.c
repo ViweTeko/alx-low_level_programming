956c91e56fb22ff456d6ca64f578ab10327d2b2d
ViweTeko <iamviwe.teko@gmail.com>
2023-07-24 08:45:30 -0700

    Reverse

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * rev_string - reverses the string
+ *
+ * @s: the string
+ */
+
+void rev_string(char *s)
+{       
+        int t, u = 0;
+        char v = s[0];
+        
+	while (s[u] != '\0')
+		++u;
+
+        for (t = 0; t < u; ++t)
+        {
+		--u;
+		v = s[t];
+		s[t] = s[u];
+		s[u] = v;
+	}
+}
