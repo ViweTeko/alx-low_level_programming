bff5e75190a5faf354b3411c857b5535483497db
ViweTeko <iamviwe.teko@gmail.com>
2023-07-24 09:08:06 -0700

    Puts 2

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * puts2 - prints every other character of string
+ *
+ * @str: string character
+ */
+
+void puts2(char *str)
+{
+	int n;
+
+	for (n = 0; str[n] != '\0'; ++n)
+	{
+		if (n % 2 == 0)
+			_putchar(str[n]);
+	}
+
+	_putchar('\n');
+}
