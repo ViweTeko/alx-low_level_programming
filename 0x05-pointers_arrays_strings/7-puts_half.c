34d6f1c644d6e034688cc9e024269f8c2fbf548c
ViweTeko <iamviwe.teko@gmail.com>
2023-07-24 12:44:42 -0700

    Half and half

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * puts_half - prints half a string
+ *
+ * @str: string input
+ */
+
+void puts_half(char *str)
+{
+	int e, f, g = 0;
+
+	for (e = 0; str[e] != '\0'; ++e)
+		++g;
+
+	f = (g / 2);
+
+	if ((f  % 2) == 1)
+		f = ((g + 1) / 2);
+
+	for (e = f; str[e] != '\0'; ++e)
+		_putchar(str[e]);
+
+	_putchar('\n');
+}
