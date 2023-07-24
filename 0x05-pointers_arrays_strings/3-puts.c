ef083c9abedeb311bca89f6c051d47532cac3952
ViweTeko <iamviwe.teko@gmail.com>
2023-07-24 07:12:30 -0700

    Puts

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _puts - a function that prints a string
+ *
+ * @str: the string input
+ *
+ * Return: 0
+ */
+
+void _puts(char *str)
+{
+	int a = 0;
+
+	while (str[a] != '\0')
+	{
+		_putchar(str[a])
+		++a;
+	}
+	_putchar('\n');
+}
