9d6f277e71e02c8da2b0624bd29bcf1267580453
ViweTeko <iamviwe.teko@gmail.com>
2023-07-26 00:33:24 -0700

    String cat

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * _strcat - appends the src string to dest string
+ *
+ * @dest: the string that will include appended string
+ * @src: the string that will append to dest
+ */
+
+char *_strcat(char *dest, char *src)
+{
+	int a, b;
+
+	while (dest[b] != '\0')
+		++b;
+
+	for (a = 0; src[a] != '\0'; ++a)
+	{
+		dest[b] = src[a];
+		++b;
+	}
+
+	dest[b] = src[a];
+
+	return (dest);
+}
