3196a99c2dbd3cc0f35644d700d8d8bb9ba1f828
ViweTeko <iamviwe.teko@gmail.com>
2023-07-27 12:45:57 -0700

    Print number

---
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -0,0 +1,25 @@
+#include "main.c"
+
+/**
+ * print_number - prints an integer
+ *
+ * @n: integer to be printed
+ *
+ * Return: 0
+ */
+
+void print_number(int n)
+{
+	int o = n;
+
+	if (n < 0)
+	{
+		_putchar('-');
+		o = -n;
+	}
+
+	if (o / 10 != 0)
+		print_number(o / 10);
+
+	_putchar(o + '0');
+}
