43f39a321a3b5055426ffdcbefb8e877ae8d1055
ViweTeko <iamviwe.teko@gmail.com>
2023-08-02 20:49:35 +0200

    String length

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -5,17 +5,19 @@
  *
  * @s: String
  *
- * Return: strlen
+ * Return: r
  */
 
 int _strlen_recursion(char *s)
 {
-	if (s[0] == '\0')
-		return (0);
+	int r = 0;
 
-	if (s[1] == '\0')
-		return (1);
-	else
-		return (_strlen_recursion(&s[1] + 1));
+	if (*s)
+	{
+		++r;
+		r += _strlen_recursion(s + 1);
+	}
+
+	return (r);
 
 }
