beaf12ea7bb74c6fcbb8e7e466b07c30c705d534
ViweTeko <iamviwe.teko@gmail.com>
2023-09-04 19:48:09 +0200

    Files

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * create_file - creates a file
+ * @filename: the name of file
+ * @text_content: writes to file
+ *
+ * Return: 1 or -1
+ */
+
+int create_file(const char *filename, char *text_context)
+{
+	int fn, wr, ln = 0;
+
+	if (filename == NULL)
+		return (-1);
+	if (text_content != NULL)
+	{
+		while (text_content[ln])
+			++ln;
+	}
+
+	fn = open(filename, 0_CREAT | 0_RDWR | 0_TRUNC, 0600);
+	wr write(fn, text_content, ln);
+	if (fn == -1 || wr == -1)
+		return (-1);
+	close(fn);
+
+	return (1);
+}
