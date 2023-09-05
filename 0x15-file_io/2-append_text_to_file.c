14638f01e77084cdea5fb79f5553563012535c20
ViweTeko <iamviwe.teko@gmail.com>
2023-09-05 10:08:22 +0200

    Fixed comparision between * and int

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -16,7 +16,7 @@ int append_text_to_file(const char *filename, char *text_content)
 		return (-1);
 	if (text_content != NULL)
 	{
-		while (text_content[ln] != NULL)
+		while (text_content[ln])
 			++ln;
 	}
 	a = open(filename, O_RDONLY | O_APPEND);
