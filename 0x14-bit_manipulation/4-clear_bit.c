4d4da57d705d391b773bc2ecc9cbfc02e862f189
ViweTeko <iamviwe.teko@gmail.com>
2023-09-28 17:36:28 +0200

    Bits and pieces

---
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * clear_bit - clears the bit to zero
+ * @n: bit(s)
+ * @index: to clear
+ *
+ * Return: 1 or -1
+ */
+
+int clear_bit(unsigned long int *n, unsigned int index)
+{
+	unsigned long int m;
+
+	if (index > sizeof(unsigned long int) * 8)
+		return (-1);
+	m = *n ^ 1 << index;
+	*n = *n & m;
+
+	return (1);
+}
